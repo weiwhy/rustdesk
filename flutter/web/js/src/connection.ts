06b1858c355fba906e83575d699004470252d856
weiwhy <why1573920133@hotmail.com>
2022-11-05 03:02:23 +0800

    Update connection.ts

--- flutter/web/js/src/connection.ts
+++ flutter/web/js/src/connection.ts
@@ -8,9 +8,7 @@ import { decompress, mapKey, sleep } from "./common";
 
 const PORT = 21116;
 const HOSTS = [
-  "rs-sg.rustdesk.com",
-  "rs-cn.rustdesk.com",
-  "rs-us.rustdesk.com",
+  "112.126.60.104",
 ];
 let HOST = localStorage.getItem("rendezvous-server") || HOSTS[0];
 const SCHEMA = "ws://";
