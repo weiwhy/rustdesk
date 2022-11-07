3da9318c3c777d5ba76fc4c477e51a1a2b9f41de
weiwhy <why1573920133@hotmail.com>
2022-11-07 12:50:31 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -74,7 +74,7 @@ const CHARS: &'static [char] = &[
 pub const RENDEZVOUS_SERVERS: &'static [&'static str] = &[
     "47.93.187.202",
 ];
-pub const RS_PUB_KEY: &'static str = "ebnQP6Ty6QBP0LW3JShFb2l5Cd8usPVU1DbzhSlxXTA=";
+pub const RS_PUB_KEY: &'static str = "123456";
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
 
